af2c9381b5a5ad00e69dcfe250990fd0f331a986
lixiang <348816986@qq.com>
2016-07-22 19:28:40 +0800

    在火狐以及safari上添加图层狂报错的bug

--- public/ide/modules/ide/js/projectService.js
+++ public/ide/modules/ide/js/projectService.js
@@ -160,12 +160,8 @@ ideServices
                         this.backgroundImg.left=(cos*deltaLeft+sin*deltaTop)/layerNode.getScaleX()-this.width/2;
 
                     }
-                    
+
                     ctx.drawImage(this.backgroundImg.element,
-                        0,
-                        0,
-                        this.backgroundImg.widht,
-                        this.backgroundImg.height,
                         this.backgroundImg.left,
                         this.backgroundImg.top,
 
@@ -175,7 +171,7 @@ ideServices
                 }
                 catch(err){
                     console.log('错误描述',err);
-                    toastr.warning('渲染Layer出错');
+                    //toastr.warning('渲染Layer出错');
                 }
 
             }
@@ -992,6 +988,17 @@ ideServices
                         ctx.drawImage(this.pointerImageElement, 0, 0,pointerImgWidth,pointerImgHeight);
                         ctx.restore();
                     }
+                    //将图片超出canvas的部分裁剪
+                    this.clipTo=function(ctx){
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.rect(-this.width / 2,
+                            -this.height / 2,
+                            this.width,
+                            this.height);
+                        ctx.closePath();
+                        ctx.restore();
+                    };
                 }
                 catch(err){
                     console.log('错误描述',err);
