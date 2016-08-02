86149ccbbeea2c82fba9782c265eb1b371ff3cb1
zzen1ss <940476656@qq.com>
2016-08-02 09:46:50 +0800

    support for closing window popup on local version, fix for xp

--- public/ide/modules/ide/js/closeBrowser.js
+++ public/ide/modules/ide/js/closeBrowser.js
@@ -1,13 +1,3 @@
 /**
  * Created by lixiang on 16/4/18.
  */
- window.addEventListener("beforeunload", function(event) {
-     var status =  document.getElementById("saveFlag").value;
-     console.log(flag,typeof  flag);
-     if(status=='false'){
-         event.returnValue="请确定已保存您的工程";
-     }
-     else{
-         console.log('projects have been saved');
-     }
- });
