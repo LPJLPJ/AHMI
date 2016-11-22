de85d2e5d2154e956cf0938ba9cc97fe1c607ab4
changecheng <zeyucheng@gmail.com>
2016-11-22 12:27:01 +0800

    restart-build

--- public/general/js/versionTag.js
+++ public/general/js/versionTag.js
@@ -6,7 +6,7 @@
         var versionNum ='1.3';
         window.ideVersion =  versionNum;
         var versionTag = document.createElement('div');
-        versionTag.id = 'version-tag';
+        versionTag.id = 'version-tag' ;
         versionTag.innerHTML = versionNum;
         versionTag.setAttribute('style',"position:fixed;right:5px;bottom:10%;padding:0.2em;font:24px helvetica arial;z-index:99");
         var body = document.body;
