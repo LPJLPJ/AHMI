42e69cc4da54776fb352bef7816b4013a9593758
USTCLX <348816986@qq.com>
2017-04-07 16:43:22 +0800

    button and buttonGroup commands in es6

--- public/ide/modules/ide/js/widgetModel/es6/widgetCommands.js
+++ public/ide/modules/ide/js/widgetModel/es6/widgetCommands.js
@@ -27,8 +27,8 @@
             var(b,'this.mode')
             if(b==0){
                 set('this.layers.0.hidden',1)
-                set('this.layers.1.hidden0',0)
-                setTag(0);
+                set('this.layers.1.hidden',0)
+                setTag(0)
             }else{
                 var(c,0)
                 getTag(c)
@@ -59,13 +59,72 @@
                     set('this.layer.0.hidden',0)
                     set('this.layer.1.hidden',1)
                 }
-            }else{
-
             }
         `
-
     };
+    WidgetCommands['ButtonGroup'] = {
+        onMouseDown:`
+            var(a,0)
+            var(b,0)
+            var(c,0)
+            set(c,'this.layers.length')
+            minus(c,2)
+            set(a,'this.innerX')
+            set(b,'this.innerY')
+            var(lx,0)
+            var(ly,0)
+            var(lw,0)
+            var(lh,0)
+            var(rx,0)
+            var(ry,0)
+            while(c>=0){
+                set(lx,'this.layers.c.x')
+                set(ly,'this.layers.c.y')
+                set(lw,'this.layers.c.width')
+                set(lh,'this.layers.c.height')
+                set(rx,lx)
+                set(ry,ly)
+                add(rx,lw)
+                add(ry,lh)
+                if(a>=lx){
+                    if(rx>a){
+                        if(b>=ly){
+                            if(ry>b){
+                                divide(c,2)
+                                setTag(c)
+                                set(c,0)
+                            }
+                        }
+                    }
+                }
+                minus(c,2)
+            }
+        `,
+        onTagChange:`
+            var(a,0)
+            var(b,0)
+            var(c,0)
+            set(a,'this.lalyers.length')
+            set(c,a)
+            divide(c,2)
+            while(a>0){
+                minus(a,1)
+                set('this.layers.a.hidden',1)
+                minus(a,1)
+                set('this.layers.a.hidden',0)
+            }
+            getTag(a)
+            if(a>=0){
+                if(c>a){
+                    multiply(a,2)
+                    set('this.layers.a.hidden',1)
+                    add(a,1)
+                    set('this.layers.a.hidden',0)
+                }
+            }
 
+        `
+    };
     return WidgetCommands;
 
 }));
@@ -84,5 +143,4 @@
  * mod(a,2)
  * set('this.layers.1.hidden',1)
  */
-
 
