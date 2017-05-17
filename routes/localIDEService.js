9301d18ae5366b83231d0118148f3be4187da858
USTCLX <348816986@qq.com>
2017-05-17 17:43:44 +0800

    可以上传生成的压缩包，并创建工程了！

--- routes/localIDEService.js
+++ routes/localIDEService.js
@@ -11,6 +11,7 @@ var path = require('path');
 var errHandler = require('../utils/errHandler');
 var formidable = require('formidable');
 var mkdir = require('mkdir-p');
+var extract = require('extract-zip');
 
 
 localIDEService.getCurrentVer = function(req,res){
@@ -61,7 +62,65 @@ localIDEService.uploadProject = function (req,res) {
 };
 
 localIDEService.uploadProjectZip = function(req,res){
+    if(req.session.user){
+        var tempFilesPath = path.join(__dirname,'../tempFiles'),
+            form = new formidable.IncomingForm(),
+            reDir = '',
+            fileName ='',
+            unZipFolderPath = '';
 
+        form.encoding = "utf-8";
+        form.uploadDir = tempFilesPath;
+        form.multiples = false;//设置为多文件上传
+        form.keepExtensions = true;//是否保存文件后缀
+        form.maxFieldsSize = 10*1024*1024;
+
+        form.parse(req,function(err,fields,files){
+            if(err){
+                console.log('err in parse',err);
+                errHandle(res,500,'err in parse!');
+            }
+        });
+
+        form.on('file',function (name,file) {
+            fileName = Date.now()+file.name;
+            reDir = path.join(tempFilesPath,fileName);
+            fs.renameSync(file.path,reDir);
+        });
+
+        form.on('error',function (error) {
+            console.log('error',error);
+        });
+        
+        form.on('end',function () {
+            //解压文件
+            unZipFolderPath = path.join(tempFilesPath,fileName.split('.')[0]);
+            unZipFile(reDir,unZipFolderPath,function(err){
+                if(err){
+                    errHandler(res,500,'unzip err',err);
+                }else{
+                    //检查压缩包内工程合法性
+                    checkUnZipFolder(unZipFolderPath,function(err,project){
+                        if(err){
+                            console.log('err in checkUnZip',err);
+                            errHandler(res,500,'not standard project');
+                        }else{
+                            //创建工程
+                            createProject(unZipFolderPath,project,req,function(err){
+                                if(err){
+                                    errHandler(res,500,err);
+                                }else{
+                                    res.send('ok');
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        })
+    }else{
+        errHandler(res,500,'user not login!');
+    }
 };
 
 localIDEService.returnUserType = function(req,res){
@@ -253,6 +312,107 @@ function fixProjectContent(content,id,host){
         }
     };
     return JSON.stringify(contentObj,transformSrc);
+}
+
+/**
+ * 解压压缩包
+ * @param srcPath
+ * @param destPath
+ * @param cb
+ */
+function unZipFile(srcPath,destPath,cb) {
+    //make sure destPath folder exist
+    fse.emptyDir(destPath,function(err){
+        if(err){
+            console.log('err in emptyDir',err);
+            cb&&cb(err);
+        }else{
+            //extract zip
+            extract(srcPath,{dir:destPath},function(err){
+                cb&&cb(err)
+            })
+        }
+    });
+}
+
+/**
+ * 检查上传的压缩包是否合法
+ * @param resourcePath
+ * @param cb
+ */
+function checkUnZipFolder(resourcePath,cb){
+    var jsonPath = path.join(resourcePath,'data.json'),
+        attributeArr = ['name','author','size','pageList'],
+        testResult = false,
+        myError = null,
+        project = null;
+    fs.readFile(jsonPath,'utf-8',function(err,data){
+        if(err){
+            console.log('err in read json',err);
+            myError = err;
+        }else{
+            project = JSON.parse(data);
+            testResult = attributeArr.every(function(item) {
+                return project.hasOwnProperty(item);
+            });
+            if(!testResult){
+                myError = new Error('not standard project!');
+            }
+        }
+        cb&&cb(myError,project);
+    })
+}
+
+/**
+ * 根据压缩包中的资源，创建一个工程
+ * @param resourcePath
+ * @param cb
+ */
+function createProject(resourcePath,project,req,cb){
+    var newProjectFolderPath = '';
+    var host = String(process.env.CUR_HOST).split('//')[1]||req.hostname;
+
+    var newProject = new ProjectModel();
+    newProjectFolderPath = path.join(__dirname,'../project/',String(newProject._id),'resources');
+
+    newProject.userId = req.session.user.id;
+    newProject.name =  project.name;
+    newProject.author = project.author;
+    newProject.resolution = String(project.size.width)+'*'+String(project.size.height);
+    newProject.content = fixProjectContent(JSON.stringify(project),newProject._id,host);
+
+    newProject.save(function(err){
+        if(err){
+            console.log('err in save new project',err);
+            cb&&cb(err);
+        }else{
+            fs.stat(newProjectFolderPath,function(err,stats){
+                if (stats&&stats.isDirectory&&stats.isDirectory()) {
+                    //copy resource to project resource folder
+                    fse.copy(resourcePath,newProjectFolderPath,function(err){
+                        if(err){
+                            console.log('err in copy resource to new project');
+                        }
+                        cb&&cb(err);
+                    })
+                }else{
+                    mkdir(newProjectFolderPath,function(err){
+                        if(err){
+                            console.log('err in mkdir',err);
+                            cb&&cb(err);
+                        }else{
+                            fse.copy(resourcePath,newProjectFolderPath,function(err){
+                                if(err){
+                                    console.log('err in copy resource to new project');
+                                }
+                                cb&&cb(err);
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    });
 }
 
 module.exports = localIDEService;
