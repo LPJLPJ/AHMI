9ecf3966493e401885bc9dbaf3bcd9a3b0d96296
lixiang <348816986@qq.com>
2017-08-08 16:34:08 +0800

    图层数字控件初定

--- public/ide/modules/ide/min-js/parts/attr/timerService.js
+++ public/ide/modules/ide/min-js/parts/attr/timerService.js
@@ -1,14 +1,1 @@
-/**
- * Created by lixiang on 16/3/15.
- */
-ideServices
-    .service('TimerService',[function(){
-    var timerNum=null;
-    this.setTimerNum=function(num){
-        //console.log("set num success!");
-        timerNum=num;
-    };
-    this.getTimerNum=function(){
-        return timerNum;
-    }
-}]);
+ideServices.service("TimerService",[function(){var e=null;this.setTimerNum=function(i){e=i},this.getTimerNum=function(){return e}}]);
