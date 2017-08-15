d80f2c93e2e2c2a7db399f131f7aa2dfc1ba2539
635215258@qq.com <12345asdfg>
2017-08-15 14:23:26 +0800

    修复两个bug： 1.添加控件icon遮挡。 2.属性栏select字体显示不完整。

--- public/ide/modules/ide/min-js/parts/nav/dropdownController.js
+++ public/ide/modules/ide/min-js/parts/nav/dropdownController.js
@@ -1,1 +1,37 @@
-ide.controller("addWidgetController",["$scope","Type",function(e,n){e.widgets=[{name:"图层",icon:"&#xe640;"},{name:"开关",icon:"&#xe64e;"},{name:"进度条",icon:"&#xe62a;"},{name:"仪表盘",icon:"&#xe632;"},{name:"旋转图",icon:"&#xe64f;"},{name:"时间",icon:"&#xe64d;"},{name:"数字",icon:"&#xe631;"},{name:"文本",icon:"&#xe62f;"},{name:"按钮",icon:"&#xe636;"},{name:"滑块",icon:"&#xe62e;"},{name:"按钮组",icon:"&#xe637;"},{name:"触发器",icon:"&#xe634;"},{name:"影像",icon:"&#xe650;"},{name:"开机动画",icon:"&#xe633"}],e.addWidget=function(n){var i=e.widgets;n<0||n>i.length-1||e.component.tool.addWidget(n)}}]),ide.directive("widgetIcon",[function(){return{restrict:"AE",link:function(e,n,i){n.html(i.iconfont)}}}]);
+ide.controller('addWidgetController', ['$scope', 'Type', function ($scope, Type) {
+    $scope.widgets=[
+        {name:"图层",icon:"&#xe640;"},
+        {name:"开关",icon:"&#xe64e;"},
+        {name:"进度条",icon:"&#xe62a;"},
+        {name:"仪表盘",icon:"&#xe632;"},
+        // {name:"示波器",icon:"&#xe62d;"},
+        {name:"旋转图",icon:"&#xe64f;"},
+        {name:"时间",icon:"&#xe64d;"},
+        {name:"数字",icon:"&#xe631;"},
+        {name:"文本",icon:"&#xe62f;"},
+        {name:"按钮",icon:"&#xe636;"},
+        // {name:"旋钮",icon:"&#xe628;"},
+        {name:"滑块",icon:"&#xe62e;"},
+        {name:"按钮组",icon:"&#xe637;"},
+        {name:"触发器",icon:"&#xe634;"},
+        {name:"影像",icon:"&#xe650;"},
+        {name:"开机动画",icon:"&#xe633"}
+    ];
+    $scope.addWidget = function (index) {
+        var widgets = $scope.widgets;
+        if (index<0 || index >widgets.length-1){
+            return;
+        }
+        $scope.component.tool.addWidget(index);
+    }
+}]);
+
+ide.directive('widgetIcon',[function () {
+    return {
+        restrict:'AE',
+        link: function (scope,elem,iAttrs) {
+                elem.html(iAttrs.iconfont);
+            }
+        }
+
+}])
