fb9fd12b51998464b600435345af2aa4a9986dc1
lixiang <348816986@qq.com>
2017-10-13 09:55:39 +0800

    获取backupList

--- routes/index.js
+++ routes/index.js
@@ -168,7 +168,7 @@ router.route('/project/:id/basicinfo')
     .post(projectInfo.updateProject);
 
 router.route('/project/:id/backuplist')
-    .get(projectInfo.getBackupList)
+    .get(projectInfo.getBackupList);
 
 router.route('/project/:id/editor')
     .get(projectInfo.getProjectById);
