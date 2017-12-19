146d059e3062e231fb0cfcf35683efe8da92f985
635215258@qq.com <12345asdfg>
2017-12-19 14:15:48 +0800

    图层时间控件文件添加

--- public/ide/modules/ide/js/parts/attr/imageSelectorInstanceCtl.js
+++ public/ide/modules/ide/js/parts/attr/imageSelectorInstanceCtl.js
@@ -88,7 +88,7 @@ ide.controller('ImageSelectorInstanceCtl', ['$scope','$timeout', '$uibModalInsta
             initConfigure(false,false,1,widgetInfo.tex,true,false,false);
             break;
         case Type.MyTexTime:
-            initConfigure(false,1,widgetInfo.tex,true);
+            initConfigure(false,false,1,widgetInfo.tex,true,false,false);
             break;
         default:
             initConfigure(true,false,1,widgetInfo.tex,false,false,false);
