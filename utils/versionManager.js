f4765443b980f700d5c938f6d03e98743178c831
changecheng <940476656@qq.com>
2018-03-02 09:53:33 +0800

    version control

---
+++ utils/versionManager.js
@@ -0,0 +1,30 @@
+/**
+ Version Manager for ide to manage versions of ide editor scripts
+ created by Zzen1sS on 2018/2/25
+ **/
+var fs = require('fs')
+var path = require('path')
+var VersionManager = {}
+
+var legacyBaseUrl = '../legacy'
+
+
+VersionManager.versions = []
+
+VersionManager.versionScripts = {}
+
+VersionManager.versionBaseUrl = path.join(__dirname,legacyBaseUrl)
+
+VersionManager.getAllVersions = function () {
+
+    var versionDirs = fs.readdirSync(path.join(__dirname,legacyBaseUrl)).filter(function (name) {
+        return name.indexOf('.')!==0
+    })
+    VersionManager.versions = versionDirs
+
+    for(var i=0;i<versionDirs.length;i++){
+        VersionManager.versionScripts[versionDirs[i]] =  JSON.parse(''+fs.readFileSync(path.join(__dirname,legacyBaseUrl,versionDirs[i],'ideScripts.json')))
+    }
+}
+
+module.exports =  VersionManager
