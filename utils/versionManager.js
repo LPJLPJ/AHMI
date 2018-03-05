a7db5f1f5c5d29c4626bcf865e678aba3c0948fb
changecheng <940476656@qq.com>
2018-03-05 09:36:58 +0800

    create project and modify project with ideVersion

--- utils/versionManager.js
+++ utils/versionManager.js
@@ -16,15 +16,25 @@ VersionManager.versionScripts = {}
 VersionManager.versionBaseUrl = path.join(__dirname,legacyBaseUrl)
 
 VersionManager.getAllVersions = function () {
+    var versionBaseUrlStats
+    try {
+        versionBaseUrlStats = fs.statSync(VersionManager.versionBaseUrl)
+    }catch(e){
 
-    var versionDirs = fs.readdirSync(path.join(__dirname,legacyBaseUrl)).filter(function (name) {
-        return name.indexOf('.')!==0
-    })
-    VersionManager.versions = versionDirs
-
-    for(var i=0;i<versionDirs.length;i++){
-        VersionManager.versionScripts[versionDirs[i]] =  JSON.parse(''+fs.readFileSync(path.join(__dirname,legacyBaseUrl,versionDirs[i],'ideScripts.json')))
     }
+
+    if (versionBaseUrlStats && versionBaseUrlStats.isDirectory()){
+        var versionDirs = fs.readdirSync(path.join(__dirname,legacyBaseUrl)).filter(function (name) {
+            return name.indexOf('.')!==0
+        })
+        VersionManager.versions = versionDirs
+
+        for(var i=0;i<versionDirs.length;i++){
+            VersionManager.versionScripts[versionDirs[i]] =  JSON.parse(''+fs.readFileSync(path.join(__dirname,legacyBaseUrl,versionDirs[i],'ideScripts.json')))
+        }
+    }
+
+
 }
 
 module.exports =  VersionManager
