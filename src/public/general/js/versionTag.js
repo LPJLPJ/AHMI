38c9e73fda1d03fb9a37471c843c17d0ef46a06a
changecheng <940476656@qq.com>
2018-06-25 10:21:00 +0800

    1.10.4 all compress

--- src/public/general/js/versionTag.js
+++ src/public/general/js/versionTag.js
@@ -2,7 +2,7 @@
  * Created by zzen1ss on 16/7/13.
  */
 ;(function () {
-    var versionNum ='1.10.3';
+    var versionNum ='1.10.4';
     window.ideVersion = versionNum;
     var versionTag = document.createElement('div');
     versionTag.id = 'version-tag';
