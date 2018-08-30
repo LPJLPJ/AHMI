50501694ddfdf78b9a7e1bc7f64e923f9eda6032
tang <653314653@qq.com>
2018-08-30 17:20:57 +0800

    log

--- src/public/updateLog/js/index.js
+++ src/public/updateLog/js/index.js
@@ -10,21 +10,36 @@ $(function () {
 
     //日志发布
     (function($){
-        var addLogContent = $('#add-log-content');
-        var logContentList = $('#log-content-list');
+        var releaseLogContent = $('#release-log-content');
+        var logContentList = $('#release-log-list');
 
-        addLogContent.click(function(){
-            var logType = $('#log-content-type').val(),logContent = $('#log-content-text').val();
+        releaseLogContent.on('click','#add-log-content',function(){
+            var logType = $('#release-log-type').val(),logContent = $('#release-log-content-text').val();
             if(logContent!=''){
-                var contentTpl = "<li><span class='log-type-bar'>["+logType+"]：</span><span>"+logContent+"</span></li>"
+                var contentTpl = "<li><span class='log-type-bar'>"+logType+"</span><span class='release-log-value'>"+logContent+"</span><span class='delete-release-list'>&times;</span></li>"
             }else{
                 alert('请填写内容');
                 return;
             }
             logContentList.append(contentTpl);
-            $('#log-content-text').val("");
+            $('#release-log-content-text').val("");
+        });
+
+        releaseLogContent.on('click','.delete-release-list',function(){
+            $(this).parent().remove();
+        });
+
+        var content = $(document).on('click','#confirm-release',function(){
+            var title = $('#release-log-title').val();
+            var explain = $('#release-log-explain').val();
+            logContentList.find('li').each(function(index){
+                var contentType = $(this).find('.log-type-bar').text();
+                var contentText = $(this).find('.release-log-value').text();
+                return{
+                    itemType:contentType,
+                    itemContent:contentText
+                }
+            })
         })
-    })(jQuery)
-
-
+    })(jQuery);
 });
