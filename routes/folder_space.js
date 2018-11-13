7afa2b98f16adb1b7eb77e725d94d8b8516502cf
master tang <653314653@qq.com>
2018-11-14 07:22:32 +0800

    personal info

--- routes/folder_space.js
+++ routes/folder_space.js
@@ -8,44 +8,55 @@ module.exports=function(req,res){
     var folderId = req.params.id;
     var _user = req.session.user;
     if(_user&&_user.id){
-        ClassModel.findById(folderId,function(err,folder){
+        UserModel.findById(_user.id,function(err,user){
             if(err){
-                console.log(err);
-                errHandler(res,500,'error')
-            }else{
-                var folderInfo={
-                    name:folder.name,
-                    id:folderId
-                };
-                ProjectModel.findProByClass(_user.id,folderId,function(err,projects){
+                res.status(500).end()
+            }else {
+                var userInfo = {};
+                userInfo.name = user.accountName;
+                userInfo.email = user.email;
+                userInfo.type = user.type;
+                ClassModel.findById(folderId,function(err,folder){
                     if(err){
                         console.log(err);
                         errHandler(res,500,'error')
                     }else{
-                        var processedProjects = projects.map(function (project) {
-                            var info = {};
-                            info._id = project._id;
-                            info.classId=project.classId;
-                            info.userId = project.userId;
-                            info.resolution = project.resolution;
-                            info.name = project.name;
-                            info.template = project.template;
-                            info.ideVersion = project.ideVersion;
-                            info.originalSite = project.originalSite;
-                            info.createTime = moment(project.createTime).format('YYYY-MM-DD HH:mm');
-                            info.lastModifiedTime = moment(project.lastModifiedTime).format('YYYY-MM-DD HH:mm');
-                            info.supportTouch = project.supportTouch;
-                            info.author = project.author;
-                            var thumbnail = project.thumbnail;
-                            return {
-                                projectInfo: info,
-                                thumbnail:thumbnail
-                            }
-                        });
+                        var folderInfo={
+                            name:folder.name,
+                            id:folderId
+                        };
+                        ProjectModel.findProByClass(_user.id,folderId,function(err,projects){
+                            if(err){
+                                console.log(err);
+                                errHandler(res,500,'error')
+                            }else{
+                                var processedProjects = projects.map(function (project) {
+                                    var info = {};
+                                    info._id = project._id;
+                                    info.classId=project.classId;
+                                    info.userId = project.userId;
+                                    info.resolution = project.resolution;
+                                    info.name = project.name;
+                                    info.template = project.template;
+                                    info.ideVersion = project.ideVersion;
+                                    info.originalSite = project.originalSite;
+                                    info.createTime = moment(project.createTime).format('YYYY-MM-DD HH:mm');
+                                    info.lastModifiedTime = moment(project.lastModifiedTime).format('YYYY-MM-DD HH:mm');
+                                    info.supportTouch = project.supportTouch;
+                                    info.author = project.author;
+                                    var thumbnail = project.thumbnail;
+                                    return {
+                                        projectInfo: info,
+                                        thumbnail:thumbnail
+                                    }
+                                });
 
-                        res.render('login/folderSpace.html',{
-                            folder:folderInfo,
-                            projects:processedProjects
+                                res.render('login/folderSpace.html',{
+                                    folder:folderInfo,
+                                    projects:processedProjects,
+                                    userInfo:userInfo
+                                });
+                            }
                         });
                     }
                 });
