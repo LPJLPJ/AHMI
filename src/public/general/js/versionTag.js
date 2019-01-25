36478096e89fa1c02129de2bcfe01c1c04a443aa
changecheng <940476656@qq.com>
2019-01-25 09:37:58 +0800

    release version: 1.10.7_build_1.25.9.37

--- src/public/general/js/versionTag.js
+++ src/public/general/js/versionTag.js
@@ -2,7 +2,7 @@
  * Created by zzen1ss on 16/7/13.
  */
 ;(function () {
-    var versionNum = '1.10.7_build_1.22.15.29';
+    var versionNum = '1.10.7_build_1.25.9.37';
     window.ideVersion = versionNum;
     var versionTag = document.createElement('div');
     versionTag.id = 'version-tag';
