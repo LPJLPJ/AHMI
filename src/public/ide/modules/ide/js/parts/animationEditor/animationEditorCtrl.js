34b21df15edd2548ed8c14df53abe6bc49615dc2
changecheng <940476656@qq.com>
2019-02-20 15:58:52 +0800

    support timingFun

--- src/public/ide/modules/ide/js/parts/animationEditor/animationEditorCtrl.js
+++ src/public/ide/modules/ide/js/parts/animationEditor/animationEditorCtrl.js
@@ -73,6 +73,43 @@ ide.controller('AnimationEditorCtrl', ['$scope','$timeout', 'ProjectService','Ty
         onAttributeChanged()
     }
 
+    //timingFuns
+
+    var EasingFunctions = {
+        // no easing, no acceleration
+        linear: function (t) { return t },
+        // accelerating from zero velocity
+        // easeInQuad: function (t) { return t*t },
+        // // decelerating to zero velocity
+        // easeOutQuad: function (t) { return t*(2-t) },
+        // // acceleration until halfway, then deceleration
+        // easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+        // accelerating from zero velocity
+        easeInCubic: function (t) { return t*t*t },
+        // decelerating to zero velocity
+        easeOutCubic: function (t) { return (--t)*t*t+1 },
+        // acceleration until halfway, then deceleration
+        easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+        // accelerating from zero velocity
+        // easeInQuart: function (t) { return t*t*t*t },
+        // // decelerating to zero velocity
+        // easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+        // // acceleration until halfway, then deceleration
+        // easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+        // // accelerating from zero velocity
+        // easeInQuint: function (t) { return t*t*t*t*t },
+        // // decelerating to zero velocity
+        // easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+        // // acceleration until halfway, then deceleration
+        // easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t },
+        // // elastic bounce effect at the beginning
+        // easeInElastic: function (t) { return (.04 - .04 / t) * Math.sin(25 * t) + 1 },
+        // // elastic bounce effect at the end
+        // easeOutElastic: function (t) { return .04 * t / (--t) * Math.sin(25 * t) },
+        // // elastic bounce effect at the beginning and end
+        // easeInOutElastic: function (t) { return (t -= .5) < 0 ? (.02 + .01 / t) * Math.sin(50 * t) : (.02 - .01 / t) * Math.sin(50 * t) + 1 }
+    }
+
     var selectedObj
 
     function onAttributeChanged(){
@@ -260,8 +297,13 @@ ide.controller('AnimationEditorCtrl', ['$scope','$timeout', 'ProjectService','Ty
         ProjectService.ChangeLayerAnimation(getCurrentAnimation())
     }
 
-    function calculateInnerAnimation(leftAnimation, rightAnimation, t){
+    function calculateInnerAnimation(leftAnimation, rightAnimation, t, timingFun){
         var result = {}
+        if(timingFun){
+            if(timingFun in EasingFunctions){
+                t = EasingFunctions[timingFun](t)
+            }
+        }
         $scope.component.animationOptions.attributes.forEach(function(attr){
             result[attr] = leftAnimation[attr] + t * (rightAnimation[attr] - leftAnimation[attr])
         });
@@ -294,7 +336,7 @@ ide.controller('AnimationEditorCtrl', ['$scope','$timeout', 'ProjectService','Ty
         if(left != -1 && right != -1){
             //calculate with left and right
             t = leftMin / (leftMin + rightMin)
-            return calculateInnerAnimation($scope.component.animation.keyFrames[left],$scope.component.animation.keyFrames[right],t)
+            return calculateInnerAnimation($scope.component.animation.keyFrames[left],$scope.component.animation.keyFrames[right],t,$scope.component.animation.keyFrames[right].timingFun)
         }else if(left != -1){
             return $scope.component.animation.keyFrames[left]
         }else if(right != -1){
