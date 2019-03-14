ae9c656d4b92967e01e3501b1d077557be4949b5
tang <653314653@qq.com>
2019-03-14 10:27:48 +0800

    fix slice delete

--- src/public/ide/modules/ide/js/parts/attr/imageSelectorInstanceCtl.js
+++ src/public/ide/modules/ide/js/parts/attr/imageSelectorInstanceCtl.js
@@ -185,7 +185,8 @@ ide.controller('ImageSelectorInstanceCtl', ['$scope','$uibModal','$timeout', '$u
                         checkByClass:checkByClass,
                         currentClass:null,
                         classes:[],
-                        classCheckStatus:false
+                        classCheckStatus:false,
+                        classReg:/#([\s\S]+)\_(\d+)+#/
                     };
                     $scope.imgResources = _.cloneDeep(imgResources);
                     $scope.batchSelectList = [];
@@ -226,7 +227,7 @@ ide.controller('ImageSelectorInstanceCtl', ['$scope','$uibModal','$timeout', '$u
                         images=selectImgs.sort(function (a, b) {
                             var x= a.name,y= b.name;
                             var reg1 = /[^\(\)]+(?=\))/g;
-                            var reg2 = /#([\s\S]+)\_(\d+)+#/;
+                            var reg2 = $scope.component.classReg;
                             var i1= parseInt(x.match(reg1) || x.match(reg2)[2]);
                             var i2= parseInt(y.match(reg1) || y.match(reg2)[2]);
                             return i1-i2;
@@ -245,7 +246,7 @@ ide.controller('ImageSelectorInstanceCtl', ['$scope','$uibModal','$timeout', '$u
                 function checkImg(imgs){
                     for(var i=0;i<imgs.length;i++){
                         var imgName=imgs[i].name;
-                        if(!imgName.match(/[^\(\)]+(?=\))/g) && !imgName.match(/#([\s\S]+)\_(\d+)+#/)){
+                        if(!imgName.match(/[^\(\)]+(?=\))/g) && !imgName.match($scope.component.classReg)){
                             return false;
                         }
                     }
@@ -255,7 +256,7 @@ ide.controller('ImageSelectorInstanceCtl', ['$scope','$uibModal','$timeout', '$u
 
                 function checkClasses() {
                     if ($scope.component.classCheckStatus) {
-                        var reg = /#([\s\S]+)\_(\d+)+#/,
+                        var reg = $scope.component.classReg,
                             check,
                             classes,
                             imgArr = imgResources.filter(function (img) {
