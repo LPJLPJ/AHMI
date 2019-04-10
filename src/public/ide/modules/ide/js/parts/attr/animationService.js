27705b8ca2bbd96e81bfe86cbaf6a6716798b9d3
changecheng <940476656@qq.com>
2019-04-10 10:35:37 +0800

    temporarily remove push transition and fadein fade out

--- src/public/ide/modules/ide/js/parts/attr/animationService.js
+++ src/public/ide/modules/ide/js/parts/attr/animationService.js
@@ -51,7 +51,7 @@ ideServices.service('AnimationService', ['ProjectService', 'Type', function (Pro
     var swipeV = new Transition('SWIPE_V','垂直滑动',1);
     var fadeInFadeOut = new Transition('FADE-IN_FADE-OUT','淡入淡出',1);
     var scale = new Transition('SCALE', '缩放', 1);
-    var transition = [noTransition,moveLR,moveRL,moveBT,moveTB,pushLR,pushRL,pushTB,pushBT,swipeH,swipeV,fadeInFadeOut,scale];
+    var transition = [noTransition,moveLR,moveRL,moveBT,moveTB,scale];
 
 
     var tempAnimation = new Animation('动画', null, null, null, 1, 1, 0, 0, 1, 1, 0);
