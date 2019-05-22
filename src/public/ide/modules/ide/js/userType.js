3d278cd7d47d53ef0a70531bee2a080cf48eac3e
changecheng <940476656@qq.com>
2019-05-22 14:32:57 +0800

    open animation and custom font feature

--- src/public/ide/modules/ide/js/userType.js
+++ src/public/ide/modules/ide/js/userType.js
@@ -13,29 +13,29 @@ ideServices.service('UserTypeService',[function(){
 
     this.getAnimationAuthor=function(){
         var animationDisabled=false;
-        switch(userType){
-            case 'ultimate':
-            case 'admin':
-                animationDisabled=false;
-                break;
-            default:
-                animationDisabled=true;
-                break;
-        }
+        // switch(userType){
+        //     case 'ultimate':
+        //     case 'admin':
+        //         animationDisabled=false;
+        //         break;
+        //     default:
+        //         animationDisabled=true;
+        //         break;
+        // }
         return animationDisabled;
     };
     this.getCustomFontAuthor=function(){
         var customFontDisabled=false;
-        switch (userType){
-            case 'pro':
-            case 'ultimate':
-            case 'admin':
-                customFontDisabled=false;
-                break;
-            default :
-                customFontDisabled=true;
-                break;
-        }
+        // switch (userType){
+        //     case 'pro':
+        //     case 'ultimate':
+        //     case 'admin':
+        //         customFontDisabled=false;
+        //         break;
+        //     default :
+        //         customFontDisabled=true;
+        //         break;
+        // }
         return customFontDisabled;
     };
 }]);
